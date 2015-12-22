2cb3812fd5d345bf274bb7540dc02600b215e034
Joseph Utecht <thenmal@gmail.com>
2015-12-22 08:46:15 -0600

    add django-rest framework

--- server/spacegoats/spacegoats/urls.py
+++ server/spacegoats/spacegoats/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
+from rest_framework import routers
 from django.contrib import admin
+from base import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
 ]
